16c10a2961029a742be910ba855c7199dafa1245
Aileen Villanueva <aileenvl@gmail.com>
2023-03-26 09:44:55 -0600

    image object detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { load } from '@tensorflow-models/coco-ssd';
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-object-detection',
+  templateUrl: './object-detection.component.html',
+  styleUrls: ['./object-detection.component.css']
 })
-export class AppComponent {
-  title = 'tensorflow-example';
+export class ObjectDetectionComponent {
+  @ViewChild('canvas') canvas: ElementRef | undefined;
+
+  isLoading: boolean = false;
+
+  predictions: any[] = [];
+
+  async detectObjects(event: any) {
+    // Set isLoading to true to show loading spinner
+    this.isLoading = true;
+
+    // Load COCO-SSD model
+    const model = await load();
+
+    // Load image from input element
+    const file = event.target.files[0];
+    const imageUrl = URL.createObjectURL(file);
+    const img = new Image();
+    img.src = imageUrl;
+
+    // Wait for image to load
+    await new Promise<void>(resolve => {
+      img.onload = () => {
+        resolve();
+      };
+    });
+    // Convert image to tensor
+    const tfImage = tf.browser.fromPixels(img);
+    // Run object detection on image
+    const predictions = await model.detect(tfImage);
+
+    // Draw bounding boxes on image using Canvas API
+    const canvas = this.canvas?.nativeElement;
+    const ctx = canvas.getContext('2d');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.clearRect(0, 0, 100, 300);
+    ctx.drawImage(img, 0, 0);
+    ctx.font = '16px Arial';
+   
+    predictions.forEach(prediction => {
+      // Draw bounding box
+      ctx.beginPath();
+      ctx.rect(...prediction.bbox);
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = 'blue';
+      ctx.fillStyle = 'white';
+      ctx.stroke();
+      ctx.fillText(
+        `${prediction.class} (${prediction.score.toFixed(2)})`,
+        prediction.bbox[0],
+        prediction.bbox[1],
+      );
+    });
+    this.predictions = predictions;
+    console.log(predictions)
+
+    // Set isLoading to false to hide loading spinner
+    this.isLoading = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ObjectDetectionComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ObjectDetectionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ObjectDetectionComponent]
 })
 export class AppModule { }
